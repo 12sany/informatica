d412f23e1d0c7d7dfe4b372fd9a5237950947ccc
12sany <62212507+12sany@users.noreply.github.com>
2020-05-29 16:59:53 +0300

    Update openApi.js

--- openApi.js
+++ openApi.js
@@ -2,7 +2,7 @@ const HSPort = 1234;
 const WSPort = 1235;
 //const ip   = "localhost";
 const ip     = "95.217.166.217";
-const path   = "/var/www/html/"; // change project path <<===|\|
+const path   = "/var/www/html/informatica"; // change project path <<===|\|
 
 const apiSHeader = {
     swaggerDefinition : {
